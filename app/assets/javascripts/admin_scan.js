658b2698efd5c36b4ab9611d0f1a17f949646dc2
mrmitchboyer <mitch@mitchboyer.com>
2014-08-13 12:11:22 -0400

    flash message

--- app/assets/javascripts/admin_scan.js
+++ app/assets/javascripts/admin_scan.js
@@ -7,6 +7,12 @@ $(function(){
       success: function(response){
         console.log(response);
         console.log("nice!");
+        $flash = $('.error-holder');
+        $flash.hide();
+        $flash.append('<p class="notice">Tweets Scanned</p>');
+        $flash.fadeIn(800, function(){
+          $flash.fadeOut(800);
+        });
 
       },
       error: function(response){
